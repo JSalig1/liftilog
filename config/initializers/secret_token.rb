899446828b5d1be4464813aa4c7ccbc11b03e45c
jsalig1 <jeremy.salig@gmail.com>
2014-10-10 11:13:15 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Liftilog::Application.config.secret_key_base = '058998f7c1ccbd41a7dd2b480f0f6a8497d280aa841b9c5ca0d2b64beebb3ff5b02ecac1fe0ba33e29a99c410d4921c233a7f2e2688b259d97d99768ffd9e1e5'
